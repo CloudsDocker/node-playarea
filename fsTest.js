45dba751946c1d4a8e589ab9a64d0a69d003aefd
todd zhang <phray.zhang@gmail.com>
2016-10-24 22:30:43 +0800

    add all

--- fsTest.js
+++ fsTest.js
@@ -19,4 +19,30 @@ var data =fs.readFileSync('hello.js');
 console.log("---------  start to read the file content Synnchronous  ----------");
 console.log("Synchrously read file: " + data.toString());
 
+// open a file 
+console.log(" --- going to Asynchronous open file ----")
+
+fs.open("test.txt","r+",function(err,data){
+	if(err){
+		return console.error(err);
+	}
+	console.log("-- File opened successfully !: ");
+});
+
+
+console.log("---- going to get file information---");
+fs.stat("test.txt",function(err,status){
+	if(err){
+		return console.error(err);
+	}
+
+	console.log(status);
+	console.log('--- get file status successfully');
+
+	// check file status
+	console.log(" -- is file? "+ status.isFile());
+	console.log(" -- is directory? "+ status.isDirectory());
+});
+
 console.log("====== Program end ======")
+
