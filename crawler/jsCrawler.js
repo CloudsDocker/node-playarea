f710d6b86bd4ec0dac3c204e302a9b9ae6fa23b6
todd zhang <phray.zhang@gmail.com>
2016-10-31 22:24:49 +0800

    update

--- crawler/jsCrawler.js
+++ crawler/jsCrawler.js
@@ -3,5 +3,13 @@ var cheerio=require('cheerio');
 var URL=require('url-parse');
 
 
-var page="www.baidu.com";
+var page="http://www.baidu.com";
 console.log("Visiting page:"+page);
+request(page,function(error,response,body){
+    if(error){
+        console.error("Error:"+error);
+    }
+
+    //check status code
+    console.log("Status code:"+response.statusCode);
+});
